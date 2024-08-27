5a54c87901928ba160b3030f33e43ff56fae28ec
Childish-Stephan <finsleystephan@gmail.com>
2024-08-27 16:50:18 +0700

    Fixed issue where options button in pause menu redirects to main menu instead of options menu

--- Script/Menus/pauseMenu.gd
+++ Script/Menus/pauseMenu.gd
@@ -20,7 +20,7 @@ func paused_or_unpaused():
 	
 func pressed_on_options_button():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Menus/options_menu.tscn")
 	
 func pressed_on_exit_button():
 	get_tree().quit()
