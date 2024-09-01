7a3bc698cf7b592d965498d6a5b4e3710baf516a
Childish-Stephan <finsleystephan@gmail.com>
2024-09-01 13:03:53 +0700

    Fix issue with option menu not redirecting back to the previous scene

--- Script/Menus/optionsMenu.gd
+++ Script/Menus/optionsMenu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+const PauseMenu = preload("res://Script/Menus/pauseMenu.gd")
+@onready var pause_menu = $"."
+
+
 func _process(delta):
 	if Input.is_action_just_pressed("backButton"):
-		pass
+		SceneManager.go_back()
+		if SceneManager.sceneArray.has("res://Levels/test_level.tscn"):
+			get_tree().paused = true
+
+			
